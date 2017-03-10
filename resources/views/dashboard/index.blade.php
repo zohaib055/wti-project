0534481b2e301a44dd4ca73f6395310cade6c316
root <zohaib055@gmail.com>
2017-03-11 02:54:56 +0500

    update

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -12,7 +12,7 @@
             <div class="icon"><i class="entypo-users"></i></div>
             <div class="num" data-start="0" data-end="83" data-postfix="" data-duration="1500" data-delay="0">0</div>
             
-            <h3>Registered users</h3>
+            <h3>Registered Members</h3>
             <p>so far in our blog, and our website.</p>
         </div>
         
